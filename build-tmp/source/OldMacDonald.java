25b9539388a0f1099b695af3adaf62d714b4e6c2
unknown <rechen@697-334-Frodo.sf.k12.internal>
2014-10-24 11:24:58 -0700

    Rebecca's OldMacDonald

--- build-tmp/source/OldMacDonald.java
+++ build-tmp/source/OldMacDonald.java
@@ -1,5 +1,21 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
 
-void setup()
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OldMacDonald extends PApplet {
+
+
+public void setup()
 {    
 	Farm manyAnimals = new Farm();
 	manyAnimals.animalSounds();
@@ -58,7 +74,7 @@ class Chick implements Animal
 	}
 	public Chick(String type, String sound1, String sound2)
 	{
-		if(Math.random()<.5)
+		if(Math.random()<.5f)
 		{
 			mySound=sound1;
 		}
@@ -114,4 +130,13 @@ class Farm
 		}       
 		System.out.println( "The cow is known as " + ((NamedCow)aBunchOfAnimals[0]).getName() );    
 	} 
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OldMacDonald" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
